c63faaaf9e463df1a6679338c53cca96cd51ce5e
Yoshiki Kobayashi <attack_no22001@yahoo.co.jp>
2023-11-23 17:57:16 +0900

    change favicon

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,4 +1,18 @@
 export default defineNuxtConfig ({
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width',
+      title: 'My App',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ],
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: https://ogp.me/ns#'
+      },
+    }
+  },
   build: {
     transpile: ['vuetify'],
   },

d0845588b3e827da4b1286568dbf1487e4d9ed92
Yoshiki Kobayashi <attack_no22001@yahoo.co.jp>
2023-11-25 21:05:09 +0900

    ナビゲーションの動きを調整

--- nuxt/components/Header.vue
+++ nuxt/components/Header.vue
@@ -14,21 +14,25 @@
       location="right"
       temporary
     >
-      <v-list>
-        <v-list-item>
-          <a href="https://twitter.com/ykyk1218_camera" target="_blank">
-            <v-icon class="text-grey-darken-1" icon="mdi-twitter"></v-icon>
-          </a>
-        </v-list-item>
-        <v-list-item>
-          <a href="https://www.instagram.com/photo_yoshiki" target="_blank">
-            <v-icon class="text-grey-darken-1" icon="mdi-instagram"></v-icon>
-          </a>
-        </v-list-item>
-        <v-list-item prepend-icon="mdi-email-outline">
-          <a class="text-grey-darken-1 text-decoration-none" href="#contact-section">Contact</a>
-        </v-list-item>
-      </v-list>
+      <div class="d-flex justify-space-between">
+        <v-list>
+          <v-list-item>
+            <a href="https://twitter.com/ykyk1218_camera" class="text-decoration-none" target="_blank">
+              <v-icon class="text-grey-darken-1" icon="mdi-twitter"></v-icon>
+            </a>
+          </v-list-item>
+          <v-list-item>
+            <a href="https://www.instagram.com/photo_yoshiki" class="text-decoration-none" target="_blank">
+              <v-icon class="text-grey-darken-1" icon="mdi-instagram"></v-icon>
+            </a>
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-email-outline">
+            <a class="text-grey-darken-1 text-decoration-none" href="#contact-section">Contact</a>
+          </v-list-item>
+        </v-list>
+        <v-icon class="text-grey-darken-1 pt-8 pr-4" icon="mdi-close" @click="drawer = false"></v-icon>
+      </div>
+      
     </v-navigation-drawer>
   </div>
 </template>
